465f4fa9c020008d17604c537b37719afec0fa1f
Rishi Raj <rishiraj.1012exp@gmail.com>
2026-07-17 10:15:26 +0200

    updated device_list.c

--- exercise1/device_list.c
+++ exercise1/device_list.c
@@ -1,69 +1,114 @@
 /*
- * Copyright (c) 2004 Topspin Communications.  All rights reserved.
- *
- * This software is available to you under a choice of one of two
- * licenses.  You may choose to be licensed under the terms of the GNU
- * General Public License (GPL) Version 2, available from the file
- * COPYING in the main directory of this source tree, or the
- * OpenIB.org BSD license below:
- *
- *     Redistribution and use in source and binary forms, with or
- *     without modification, are permitted provided that the following
- *     conditions are met:
- *
- *      - Redistributions of source code must retain the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer.
- *
- *      - Redistributions in binary form must reproduce the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer in the documentation and/or other materials
- *        provided with the distribution.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Copyright (c) 2026 Rishi Raj. All rights reserved.
  */
 
 #if HAVE_CONFIG_H
 #  include <config.h>
-#endif /* HAVE_CONFIG_H */
+#endif
 
 #include <stdio.h>
-
-#include <endian.h>
-#include <byteswap.h>
+#include <stdlib.h>
 
 #include <infiniband/verbs.h>
 #include <infiniband/arch.h>
 
-int main(int argc, char *argv[])
+static const char *node_type_str(enum ibv_node_type type)
+{
+	switch (type) {
+	case IBV_NODE_CA:
+		return "Channel Adapter";
+	case IBV_NODE_SWITCH:
+		return "Switch";
+	case IBV_NODE_ROUTER:
+		return "Router";
+	case IBV_NODE_RNIC:
+		return "RNIC";
+	case IBV_NODE_USNIC:
+		return "usNIC";
+	case IBV_NODE_USNIC_UDP:
+		return "usNIC UDP";
+	default:
+		return "Unknown";
+	}
+}
+
+static const char *transport_type_str(enum ibv_transport_type type)
+{
+	switch (type) {
+	case IBV_TRANSPORT_IB:
+		return "InfiniBand";
+	case IBV_TRANSPORT_IWARP:
+		return "iWARP";
+	case IBV_TRANSPORT_USNIC:
+		return "usNIC";
+	case IBV_TRANSPORT_USNIC_UDP:
+		return "usNIC UDP";
+	case IBV_TRANSPORT_UNSPECIFIED:
+	default:
+		return "Unspecified";
+	}
+}
+
+int main(void)
 {
 	struct ibv_device **dev_list;
-	int num_devices, i;
+	int num_devices;
 
 	dev_list = ibv_get_device_list(&num_devices);
 	if (!dev_list) {
-		perror("Failed to get IB devices list");
-		return 1;
+		perror("Failed to get RDMA device list");
+		return EXIT_FAILURE;
 	}
 
-	printf("    %-16s\t   node GUID\n", "device");
-	printf("    %-16s\t----------------\n", "------");
+	printf("Found %d RDMA device(s)\n\n", num_devices);
 
-	for (i = 0; i < num_devices; ++i) {
-		printf("    %-16s\t%016llx\n",
-		       ibv_get_device_name(dev_list[i]),
-		       (unsigned long long) ntohll(ibv_get_device_guid(dev_list[i])));
+	for (int i = 0; i < num_devices; ++i) {
+		struct ibv_device *device = dev_list[i];
+		struct ibv_context *context;
+		struct ibv_device_attr attr;
+
+		printf("Device %d\n", i);
+		printf("  Name:              %s\n",
+		       ibv_get_device_name(device));
+		printf("  Node type:         %s\n",
+		       node_type_str(ibv_get_device_node_type(device)));
+		printf("  Transport:         %s\n",
+		       transport_type_str(ibv_get_device_transport_type(device)));
+		printf("  Node GUID:         %016llx\n",
+		       (unsigned long long)
+		       ntohll(ibv_get_device_guid(device)));
+
+		context = ibv_open_device(device);
+		if (!context) {
+			perror("  Failed to open device");
+			putchar('\n');
+			continue;
+		}
+
+		if (ibv_query_device(context, &attr)) {
+			perror("  Failed to query device");
+			ibv_close_device(context);
+			putchar('\n');
+			continue;
+		}
+
+		printf("  Firmware version:  %s\n", attr.fw_ver);
+		printf("  System image GUID: %016llx\n",
+		       (unsigned long long) attr.sys_image_guid);
+		printf("  Vendor ID:         0x%06x\n", attr.vendor_id);
+		printf("  Vendor part ID:    %u\n", attr.vendor_part_id);
+		printf("  Hardware version:  %u\n", attr.hw_ver);
+		printf("  Physical ports:    %u\n", attr.phys_port_cnt);
+		printf("  Max QPs:           %d\n", attr.max_qp);
+		printf("  Max QP WRs:        %d\n", attr.max_qp_wr);
+		printf("  Max CQ entries:    %d\n", attr.max_cqe);
+		printf("  Max MRs:           %d\n", attr.max_mr);
+		printf("  Max PDs:           %d\n", attr.max_pd);
+
+		ibv_close_device(context);
+		putchar('\n');
 	}
 
 	ibv_free_device_list(dev_list);
-
-	return 0;
+	return EXIT_SUCCESS;
 }
-
